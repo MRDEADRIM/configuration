af906eb48eff7a50cb2148af6089bf8b994cdd71
MRDEADRIM <80binuthomas08@gmail.com>
2024-02-18 21:56:57 +0530

    charging status updated

--- configuration.sh
+++ configuration.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
-
 date_time () {
-    date +"%y-%b-%d %H:%M %p"
+	date +"%y-%b-%d %H:%M %p"
 }
 
 battery_percentage () {
-    cat /sys/class/power_supply/BAT0/capacity
+	cat /sys/class/power_supply/BAT0/capacity
 }
-
+battery_status (){
+	cat /sys/class/power_supply/BAT0/status
+}
 menu () {
 	while true; do
 		current_battery=$(battery_percentage)
-		if [ "$current_battery" -lt 5 ]; then
-			shutdown now
-		elif [ "$current_battery" -lt 10 ]; then
-			xsetroot -name "Critical Status"
-		elif [ "$current_battery" -lt 30 ]; then
-			xsetroot -name "Low Power"
-		else
-			xsetroot -name "$(date_time) $current_battery%"
+		battery_status=$(battery_status)
+
+
+		if [ "$battery_status" = "Charging" ]; then
+			xsetroot -name "$(date_time) $current_battery% C"
 		fi
+
+		if [ "$battery_status" = "Discharging" ]; then
+			if [ "$current_battery" -lt 5 ]; then
+				shutdown now
+			elif [ "$current_battery" -lt 10 ]; then
+				xsetroot -name "Critical Status"
+			elif [ "$current_battery" -lt 30 ]; then
+				xsetroot -name "Low Power"
+			else
+				xsetroot -name "$(date_time) $current_battery%"
+			fi
+		fi
+
 		sleep $((60 - $(date +%S) % 60))
 	done
 }
