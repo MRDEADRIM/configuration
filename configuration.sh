e9c4a99e1ef49c675a1571cd8b8d34a9bc856a2a
MRDEADRIM <80binuthomas08@gmail.com>
2024-02-21 01:02:19 +0530

    battery low audio updated

--- configuration.sh
+++ configuration.sh
@@ -1,31 +1,45 @@
-#!/bin/sh
-date_time () {
+#!/bin/bash
+
+date_time() {
 	date +"%y-%b-%d %H:%M %p"
 }
 
-battery_percentage () {
+battery_percentage() {
 	cat /sys/class/power_supply/BAT0/capacity
 }
-battery_status (){
+
+battery_status() {
 	cat /sys/class/power_supply/BAT0/status
 }
-menu () {
+
+beep_alert() {
+	frequency=$1
+	status=$2
+	while $status; do
+		paplay beep.wav
+		sleep $frequency
+		if [ "$(battery_status)" = "Charging" ]; then
+			status=false
+		fi
+	done
+}
+
+menu() {
 	while true; do
 		current_battery=$(battery_percentage)
-		battery_status=$(battery_status)
+		status=$(battery_status)
 
-
-		if [ "$battery_status" = "Charging" ]; then
+		if [ "$status" = "Charging" ]; then
 			xsetroot -name "$(date_time) $current_battery% C"
-		fi
-
-		if [ "$battery_status" = "Discharging" ]; then
+		elif [ "$status" = "Discharging" ]; then
 			if [ "$current_battery" -lt 5 ]; then
 				shutdown now
-			elif [ "$current_battery" -lt 10 ]; then
+			elif [ "$current_battery" -lt 15 ]; then
 				xsetroot -name "Critical Status"
+				beep_alert 5 true
 			elif [ "$current_battery" -lt 30 ]; then
 				xsetroot -name "Low Power"
+				beep_alert 15 true
 			else
 				xsetroot -name "$(date_time) $current_battery%"
 			fi
